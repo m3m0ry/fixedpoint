5f92671890354912ffd902806cc6e69e8f3d1021
Jan Hönig <jan.hoenig@fau.de>
2020-06-05 22:08:46 +0200

    Optimized cast operations

--- fixedpoint/arithmetic.d
+++ fixedpoint/arithmetic.d
@@ -1,46 +1,72 @@
 module fixedpoint.algorithm;
 import std.conv : to;
+import fixedpoint.fixed : Fixed;
 
 /// Before the operation the arguments are casted to the specified type
-T castedOp(string op, T, S1, S2)(const S1 rhs, const S2 lhs)
+T addTo(T, int a, V, int b, U)(Fixed!(a, V) lhs, Fixed!(b, U) rhs)
 {
-    return mixin("rhs.to!T" ~ op ~ "lhs.to!T");
+    static if (a >= b)
+    {
+        return (lhs.value.to!T + rhs.value.to!T * 10 ^^ (a - b)) / lhs.factor;
+    }
+    else
+    {
+        return (lhs.value.to!T * 10 ^^ (b - a) + rhs.value.to!T) / rhs.factor;
+    }
 }
 
-T addTo(T, S1, S2)(const S1 rhs, const S2 lhs)
+T subTo(T, int a, V, int b, U)(Fixed!(a, V) lhs, Fixed!(b, U) rhs)
 {
-    return castedOp!("+", T)(rhs, lhs);
+    static if (a >= b)
+    {
+        return (lhs.value.to!T - rhs.value.to!T * 10 ^^ (a - b)) / lhs.factor;
+    }
+    else
+    {
+        return (lhs.value.to!T * 10 ^^ (b - a) - rhs.value.to!T) / rhs.factor;
+    }
 }
 
-T subTo(T, S1, S2)(const S1 rhs, const S2 lhs)
+T mulTo(T, int a, V, int b, U)(Fixed!(a, V) lhs, Fixed!(b, U) rhs)
 {
-    return castedOp!("-", T)(rhs, lhs);
+    return (lhs.value.to!T * rhs.value.to!T) / (lhs.factor * rhs.factor);
 }
 
-T mulTo(T, S1, S2)(const S1 rhs, const S2 lhs)
+T divTo(T, int a, V, int b, U)(Fixed!(a, V) lhs, Fixed!(b, U) rhs)
 {
-    return castedOp!("*", T)(rhs, lhs);
+    static if (a >= b)
+    {
+        return (lhs.value.to!T / rhs.value.to!T) / 10 ^^ (a - b);
+    }
+    else
+    {
+        return (lhs.value.to!T / rhs.value.to!T) * 10 ^^ (b - a);
+    }
 }
-
-T divTo(T, S1, S2)(const S1 rhs, const S2 lhs)
-{
-    return castedOp!("/", T)(rhs, lhs);
-}
-
 
 unittest
 {
+    import std.stdio : writeln;
     import fixedpoint.fixed : Fixed;
     import std.math : isClose;
+
     auto op1 = Fixed!2("1.11");
     auto op2 = Fixed!2("1.11");
-    assert(isClose(castedOp!("/", double)(op1, op2), 1.0));
-    assert(isClose(divTo!double(op1, op2), 1.0));
+    auto op3 = Fixed!3("1.111");
     assert(isClose(addTo!double(op1, op2), 2.22));
     assert(isClose(subTo!double(op1, op2), 0.0));
     assert(isClose(mulTo!double(op1, op2), 1.2321));
-    assert(isClose(op1.divTo!double(op2), 1.0));
+    assert(isClose(divTo!double(op1, op2), 1.0));
     assert(isClose(op1.addTo!double(op2), 2.22));
     assert(isClose(op1.subTo!double(op2), 0.0));
     assert(isClose(op1.mulTo!double(op2), 1.2321));
+    assert(isClose(op1.divTo!double(op2), 1.0));
+    assert(isClose(addTo!double(op1, op3), 2.221));
+    assert(isClose(subTo!double(op1, op3), -0.001));
+    assert(isClose(mulTo!double(op1, op3), 1.23321));
+    assert(isClose(divTo!double(op1, op3), 0.99909990999));
+    assert(isClose(addTo!double(op3, op1), 2.221));
+    assert(isClose(subTo!double(op3, op1), 0.001));
+    assert(isClose(mulTo!double(op3, op1), 1.23321));
+    assert(isClose(divTo!double(op3, op1), 1.0009009009));
 }
